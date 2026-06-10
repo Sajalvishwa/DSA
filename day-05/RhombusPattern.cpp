f1c829274642936e1a23c37fcd6b30132f0b4ffd
Sajal vishwakarma <sajalvis8627@gmail.com>
2026-06-10 13:45:13 +0530

    add day-05 and rhombusPattern

---
+++ day-05/RhombusPattern.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+
+    int n=4;
+    for(int i=1; i<=n; i++){
+        //spaces
+        for(int j=1; j<=(n-i); j++){
+            cout<<" ";
+        }
+        //stars
+        for(int j=1; j<=i; j++){
+            cout<<"*";  
+    }
+    //stars
+    for(int k=1; k<=n-i; k++){
+        cout<<"*";
+    }
+    cout<<endl;
+    
+    }
+    return 0;
+}
