63672910f5e9fe7e6cb96487c16d96e318b22750
Sajal vishwakarma <sajalvis8627@gmail.com>
2026-06-24 12:01:35 +0530

    add day-08 and learning about pointerss

--- day-08/IntPointer.cpp
+++ day-08/IntPointer.cpp
@@ -6,6 +6,14 @@ int main()
     int a = 10;
     int *ptr = &a;
 
+    float b = 20.5;
+    float *ptr2 = &b;
+
+    cout<<"address of b is: "<<&b<<endl;
+    cout<<"value of ptr2 is: "<<ptr2<<endl;
+    cout<<"value pointed to by ptr2 is: "<<*ptr2<<endl;
+    
+
     cout<<"address of a is: "<<&a<<endl;
     cout<<"value of ptr is: "<<ptr<<endl;
     cout<<"value pointed to by ptr is: "<<*ptr<<endl;
