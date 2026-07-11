82eedeca4c0d0ba85ecfa8d52ff09f291dced250
Sajal vishwakarma <sajalvis8627@gmail.com>
2026-07-11 23:28:50 +0530

    add day-11 and InBuild Sort

---
+++ day-11/Sorting Algorithm/InBuildSort.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    sort(arr, arr+n);
+    sort(arr, arr+n, greater<int>());
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
