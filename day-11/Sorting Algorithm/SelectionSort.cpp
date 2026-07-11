5109177dbfe4030f4c40a6ab90a5d5322e19c1f9
Sajal vishwakarma <sajalvis8627@gmail.com>
2026-07-11 17:22:09 +0530

    add day-11 and Selection Sort

--- day-11/Sorting Algorithm/SelectionSort.cpp
+++ day-11/Sorting Algorithm/SelectionSort.cpp
@@ -6,7 +6,7 @@ void SelectionSort (int *arr, int n)
     for(int i=0; i<n-1; i++)
     {
         int minIndex = i;
-        for(int j=i+1; j<n; j++)
+        for(int j=i; j<n; j++)
         {
             if(arr[j] < arr[minIndex])
             {
