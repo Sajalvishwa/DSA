954e409fe5210dc3045acc830617985141661ec8
Sajal vishwakarma <sajalvis8627@gmail.com>
2026-07-11 08:38:21 +0530

    add day-11 and Bubble Sort

--- day-11/Sorting Algorithm/BubbleSort.cpp
+++ day-11/Sorting Algorithm/BubbleSort.cpp
@@ -14,14 +14,21 @@ void BubbleSort (int *arr, int n)
 {
     for(int i=0; i<n-1; i++)
     {
+        bool swapped = false;
         for(int j=0; j<n-i-1; j++)
         {
             if(arr[j] > arr[j+1])
             {
                 swap(arr[j], arr[j+1]);
+                swapped = true;
             }
         }
-    }print(arr,n);
+        if(!swapped)
+        {
+            break;
+        }
+    }
+    print(arr, n);
 }
 
 int main ()
