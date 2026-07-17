bb01c1f2a9b1f384ac0117c5076bc9113b9708e0
Sajal vishwakarma <sajalvis8627@gmail.com>
2026-07-17 10:00:06 +0530

    add day-12 and DiagonalSum

---
+++ day-12/2DArray/DiagonalSum.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int DiagonalSum(int arr[3][3], int n)
+{
+    int sum =0;
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(i==j || (i+j)==n-1)
+            {
+                sum+=arr[i][j];
+            }
+        }
+    }
+    cout<<"Sum of diagonal elements is: "<<sum<<endl;
+
+    return sum;
+}
+
+int main()
+{
+    int arr[3][3]={{1,2,3},
+                 {4,5,6},
+                 {7,8,9}};
+    
+    DiagonalSum(arr, 3);
+}
